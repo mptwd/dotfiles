10191bf21f7dfba80b775497d53267c4026dfdc3
Marc Portwood <marcportwood@gmail.com>
2024-05-14 20:01:51 +0200

    nvim -> options : pas sur de son utilité

---
+++ config/.config/nvim/lua/config/options.lua
@@ -0,0 +1,22 @@
+local opt = vim.opt
+
+-- Interesting Options --
+
+opt.inccommand = "split"
+
+opt.smartcase = true
+opt.ignorecase = true
+
+opt.number = true
+opt.relativenumber = true
+
+opt.splitbelow = true
+opt.splitright = true
+
+opt.signcolumn = "yes"
+opt.shada = { "'10", "<0", "s10", "h" }
+
+opt.clipboard = "unnamedplus"
+
+-- Don't have `o` add a comment
+opt.formatoptions:remove "o"
